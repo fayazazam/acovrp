f09c37787076c8826f6e00e134636b15071fd03a
Cas Dekkers <casdekkers@gmail.com>
2017-11-10 18:23:29 +0100

    Delete custom random graph, add networkx random graph

--- acovrp.py
+++ acovrp.py
@@ -1,39 +1,12 @@
 #!python2
 
-from random import randint
-import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
 
-class graph():
-	def __init__(self):
-		self.n = randint(2,10) # number of nodes
-		self.a = randint(1,(self.n*(self.n-1)/2)); # number of edges
-		self.matrix = np.empty((self.n, self.n))
-		rand = 2
-		for i in xrange(0, self.n):
-			for j in xrange(0, self.n):
-				if i != j:
-					if rand > randint(0,rand):
-						weight = randint(1,self.n)
-						self.matrix[i][j] = weight
-						self.matrix[j][i] = weight
-					else:
-						self.matrix[i][j] = -1
-						self.matrix[j][i] = -1
-				else:
-					self.matrix[i][j] = -1
-					self.matrix[j][i] = -1
-		print self.matrix
-
-class customer():
-	def __init__(self, demand):
-		self.demand = demand
-
-class ant():
-	def __init__(self):
-		self.current = 0
-		self.next = 0
-		self.capacity = 10
-		self.distance = 0
-
-if __name__=='__main__':
-	G = graph()
+if __name__ == '__main__':
+	visualize = True
+	G = nx.fast_gnp_random_graph(10, 0.2, 42, True)
+	print nx.all_pairs_node_connectivity(G)
+	if visualize:
+		nx.draw_shell(G)
+		plt.show()
