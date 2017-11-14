518319f942665bc5d53ad0629101302e65028d12
Hearrtbeatt <juulysa@hotmail.com>
2017-11-14 16:50:28 +0100

    Added function for random walks (wip)

--- acovrp.py
+++ acovrp.py
@@ -2,12 +2,14 @@
 
 from tsplibparser import TSPLIBParser
 import matplotlib.pyplot as plt
+from random import randint
 import networkx as nx
 import numpy as np
 
 class Vertex(object):
 	def __init__(self, id):
 		self.id = id
+		#coordinates?
 
 class Edge(object):
 	def __init__(self, i, j, weight):
@@ -19,6 +21,15 @@ class Graph(object):
 		self.V = set()
 		self.E = set()
 
+	def __str__(self):
+		return V.id
+
+class Ant(object):
+	capacity = 0
+
+	def __init__(self, id):
+		self.id = id
+
 def randomGraph(n=20, p=0.25):
 	G = Graph()
 
@@ -27,9 +38,10 @@ def randomGraph(n=20, p=0.25):
 
 	for i in xrange(0,n):
 		for j in xrange(i,n):
-			if np.random.rand() < p:
-				weight = np.random.rand()
-				G.E.add(Edge(i,j,weight))
+			#if np.random.rand() < p:
+			weight = np.random.rand()
+			G.E.add(Edge(i,j,weight))
+			G.E.add(Edge(j,i,weight))
 
 	return G
 
@@ -55,9 +67,65 @@ def drawGraph(G):
 	plt.axis('off')
 	plt.show()
 
+def walkingAnt(G, depot, i):
+	print "Walking"
+
+	currentNode = depot
+	nodesSeen = []
+	amountDelivered = 0
+	route = []
+
+	nodesSeen.append(depot)
+
+
+	while amountDelivered < Ant.capacity:
+		randomNode = randint(0, len(G.V))
+		if (randomNode not in nodesSeen and randomNode != currentNode):
+			amountDelivered = 5 #value of edge between node i and j
+			route.append(randomNode)
+			nodesSeen.append(randomNode) 
+			currentNode = randomNode
+		else:
+			break	
+	print amountDelivered
+	print route
+
+
+
 if __name__ == '__main__':
-	visualize = True
+	beta = 2.3
+	alpha = 0.1
+	numberOfAnts = 2
+	Ant.capacity = 100
+
+	visualize = False
 	G = randomGraph()
 	
 	if visualize:
 		drawGraph(G)
+
+	#Set one of the nodes in the set as the depot
+	depot = randint(0, len(G.V))
+	print depot
+
+	#Create a list of pre-set length containing the ants
+	ants = []
+	for i in xrange(0, numberOfAnts):
+		ants.append(Ant(i))
+
+	for i in range(0, len(ants)):
+		walkingAnt(G, depot, ants[i])
+		print "----next----\n"
+
+	
+
+
+
+
+
+
+
+
+
+
+
