cf8627698089dce866f2c93c610a672f52e365df
C.H.J. Dekkers <s1145347@leidenuniv.nl>
2017-11-14 16:59:44 +0100

    Finished reading spec, started reading data sections

--- tsplibparser.py
+++ tsplibparser.py
@@ -19,100 +19,93 @@ class TSPLIBParser(object):
 
 	def parse(self):
 		re_spec = r"(\w+)\s*\:\s*(.+)\n"
-
+		re_data = r"\s*([A-Z_]+)\s*\n"
+		re_node_coords = r"\s*(\d+)\s+(\-?\d+(\.\d+)?)\s+(\-?\d+(\.\d+)?)\s*\n"
 		sections = dict.fromkeys(['node_coords', 'depots', 'demands', 'edge_data', 'fixed_edges', 'display_data', 'tours', 'edge_weights',], False)
+		counter = 0
 
 		with open(self.filename, 'r') as file:
 			for line in file:
-				if re.match(re_spec, line):
-					m = re.match(re_spec, line)
-					if m.group(1) == 'NAME':
-						self.data['name'] = m.group(2)
-					elif m.group(1) == 'TYPE':
-						TYPE = re.match(r"\s*(\w+)\s*", m.group(2))
-						assert TYPE.group(1) in TSPLIBParser.TYPES
-						self.data['type'] = TYPE.group(1)
-					elif m.group(1) == 'COMMENT':
-						self.data['comment'] = m.group(2)
-					elif m.group(1) == 'DIMENSION':
-						self.data['dimension'] = int(m.group(2))
-					elif m.group(1) == 'CAPACITY':
-						self.data['capacity'] = int(m.group(2))
-					elif m.group(1) == 'EDGE_WEIGHT_TYPE':
-						EDGE_WEIGHT_TYPE = re.match(r"\s*(\w+)\s*", m.group(2))
-						assert EDGE_WEIGHT_TYPE.group(1) in TSPLIBParser.EDGE_WEIGHT_TYPES
-						self.data['edge_weight_types'] = EDGE_WEIGHT_TYPE.group(1)
-					elif m.group(1) == 'EDGE_WEIGHT_FORMAT':
-						assert m.group(2) in TSPLIBParser.EDGE_WEIGHT_FORMATS
-						self.data['edge_weight_format'] = m.group(2)
-					elif m.group(1) == 'EDGE_DATA_FORMAT':
-						assert m.group(2) in TSPLIBParser.EDGE_DATA_FORMATS
-						self.data['edge_data_format'] = m.group(2)
-					elif m.group(1) == 'NODE_COORD_TYPE':
-						assert m.group(2) in TSPLIBParser.NODE_COORD_TYPES
-						self.data['node_coord_type'] = m.group(2)
-					elif m.group(1) == 'DISPLAY_DATA_TYPE':
-						assert m.group(2) in TSPLIBParser.DISPLAY_DATA_TYPES
-						self.data['display_data_type'] = m.group(2)
-
-				# if any(section in sections):
-				# 	if sections['node_coords']:
-						
-				# else:					
-				# 	line = line.split("\n")[0] # remove line feed at the end
-				# 	line = line.split(": ", 1) # split on colon
-				# 	if len(line) == 2:
-				# 		if 'NAME' in line[0]:
-				# 			self.data['name'] = line[1]
-				# 		elif 'EDGE_WEIGHT_TYPE' in line[0]:
-				# 			for EDGE_WEIGHT_TYPE in TSPLIBParser.EDGE_WEIGHT_TYPES:
-				# 				if EDGE_WEIGHT_TYPE in line[1]:
-				# 					self.data['edge_weight_type'] = EDGE_WEIGHT_TYPE
-				# 		elif 'TYPE' in line[0]:
-				# 			for TYPE in TSPLIBParser.TYPES:
-				# 				if TYPE in line[1]:
-				# 					self.data['type'] = TYPE
-				# 		elif 'COMMENT' in line[0]:
-				# 			self.data['comment'] = line[1]
-				# 		elif 'DIMENSION' in line[0]:
-				# 			self.data['dimension'] = int(line[1])
-				# 		elif 'CAPACITY' in line[0]:
-				# 			self.data['capacity'] = int(line[1])
-				# 		elif 'EDGE_WEIGHT_FORMAT' in line[0]:
-				# 			for EDGE_WEIGHT_FORMAT in TSPLIBParser.EDGE_WEIGHT_FORMATS:
-				# 				if EDGE_WEIGHT_FORMAT in line[1]:
-				# 					self.data['edge_weight_format'] = EDGE_WEIGHT_FORMAT
-				# 		elif 'EDGE_DATA_FORMAT' in line[0]:
-				# 			for EDGE_DATA_FORMAT in TSPLIBParser.EDGE_DATA_FORMATS:
-				# 				if EDGE_DATA_FORMAT in line[1]:
-				# 					self.data['edge_data_format'] = EDGE_DATA_FORMAT
-				# 		elif 'NODE_COORD_TYPE' in line[0]:
-				# 			for NODE_COORD_TYPE in TSPLIBParser.NODE_COORD_TYPES:
-				# 				if NODE_COORD_TYPE in line[1]:
-				# 					self.data['node_coord_type'] = NODE_COORD_TYPE
-				# 		elif 'DISPLAY_DATA_TYPE' in line[0]:
-				# 			for DISPLAY_DATA_TYPE in TSPLIBParser.DISPLAY_DATA_TYPES:
-				# 				if DISPLAY_DATA_TYPE in line[1]:
-				# 					self.data['display_data_type'] = DISPLAY_DATA_TYPE
-				# 	elif len(line) == 1:
-				# 		if 'EOF' in line:
-				# 			break;
-				# 		elif 'NODE_COORD_SECTION' in line:
-				# 			sections['node_coords'] = True
-				# 		elif 'DEPOT_SECTION' in line:
-				# 			sections['depots'] = True
-				# 		elif 'DEMAND_SECTION' in line:
-				# 			sections['demands'] = True
-				# 		elif 'EDGE_DATA_SECTION' in line:
-				# 			sections['edge_data'] = True
-				# 		elif 'FIXED_EDGES_SECTION' in line:
-				# 			sections['fixed_edges'] = True
-				# 		elif 'DISPLAY_DATA_SECTION' in line:
-				# 			sections['display_data'] = True
-				# 		elif 'TOUR_SECTION' in line:
-				# 			sections['tours'] = True
-				# 		elif 'EDGE_WEIGHT_SECTION' in line:
-				# 			sections['edge_weights'] = True
-
-					
+				if any(value for key, value in sections.iteritems()):
+					if sections['node_coords']:
+						if counter < self.data['dimension']:
+							m = re.match(re_spec, line)
+							print m.group(2)
+							counter += 1
+							continue
+						else:
+							counter = 0
+							sections['node_coords'] = False
+					elif sections['depots']:
+						print 'depots'
+						sections['depots'] = False
+					elif sections['demands']:
+						print 'demands'
+						sections['demands'] = False
+					elif sections['edge_data']:
+						print 'edge_data'
+						sections['edge_data'] = False
+					elif sections['fixed_edges']:
+						print 'fixed_edges'
+						sections['fixed_edges'] = False
+					elif sections['display_data']:
+						print 'display_data'
+						sections['display_data'] = False
+					elif sections['tours']:
+						print 'tours'
+						sections['tours'] = False
+					elif sections['edge_weights']:
+						print 'edge_weights'
+						sections['edge_weights'] = False
+				else:					
+					if re.match(re_spec, line):
+						m = re.match(re_spec, line)
+						if m.group(1) == 'NAME':
+							self.data['name'] = m.group(2)
+						elif m.group(1) == 'TYPE':
+							TYPE = re.match(r"\s*(\w+)\s*", m.group(2))
+							assert TYPE.group(1) in TSPLIBParser.TYPES
+							self.data['type'] = TYPE.group(1)
+						elif m.group(1) == 'COMMENT':
+							self.data['comment'] = m.group(2)
+						elif m.group(1) == 'DIMENSION':
+							self.data['dimension'] = int(m.group(2))
+						elif m.group(1) == 'CAPACITY':
+							self.data['capacity'] = int(m.group(2))
+						elif m.group(1) == 'EDGE_WEIGHT_TYPE':
+							EDGE_WEIGHT_TYPE = re.match(r"\s*(\w+)\s*", m.group(2))
+							assert EDGE_WEIGHT_TYPE.group(1) in TSPLIBParser.EDGE_WEIGHT_TYPES
+							self.data['edge_weight_types'] = EDGE_WEIGHT_TYPE.group(1)
+						elif m.group(1) == 'EDGE_WEIGHT_FORMAT':
+							assert m.group(2) in TSPLIBParser.EDGE_WEIGHT_FORMATS
+							self.data['edge_weight_format'] = m.group(2)
+						elif m.group(1) == 'EDGE_DATA_FORMAT':
+							assert m.group(2) in TSPLIBParser.EDGE_DATA_FORMATS
+							self.data['edge_data_format'] = m.group(2)
+						elif m.group(1) == 'NODE_COORD_TYPE':
+							assert m.group(2) in TSPLIBParser.NODE_COORD_TYPES
+							self.data['node_coord_type'] = m.group(2)
+						elif m.group(1) == 'DISPLAY_DATA_TYPE':
+							assert m.group(2) in TSPLIBParser.DISPLAY_DATA_TYPES
+							self.data['display_data_type'] = m.group(2)
+					elif re.match(re_data, line):
+						m = re.match(re_data, line)
+						if m.group(1) == 'EOF':
+							break
+						elif m.group(1) == 'NODE_COORD_SECTION':
+							sections['node_coords'] = True
+						elif m.group(1) == 'DEPOT_SECTION':
+							sections['depots'] = True
+						elif m.group(1) == 'DEMAND_SECTION':
+							sections['demands'] = True
+						elif m.group(1) == 'EDGE_DATA_SECTION':
+							sections['edge_data'] = True
+						elif m.group(1) == 'FIXED_EDGES_SECTION':
+							sections['fixed_edges'] = True
+						elif m.group(1) == 'DISPLAY_DATA_SECTION':
+							sections['display_data'] = True
+						elif m.group(1) == 'TOUR_SECTION':
+							sections['tours'] = True
+						elif m.group(1) == 'EDGE_WEIGHT_SECTION':
+							sections['edge_weights'] = True
 		return self.data
