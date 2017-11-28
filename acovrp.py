f446713426effab5c23aca01096dd50bf53d759d
J. Meeuwsen <s1508482@leidenuniv.nl>
2017-11-28 13:25:50 +0100

    Modified ant path selection according to paper

--- acovrp.py
+++ acovrp.py
@@ -6,6 +6,12 @@ from random import randint
 import networkx as nx
 import numpy as np
 
+ALPHA = 0.1
+BETA = 2.3
+Q0 = 0.9
+M = 25
+TAU0 = 1./784
+
 class Vertex(object):
 	def __init__(self, id, x, y):
 		self.id = id
@@ -13,14 +19,32 @@ class Vertex(object):
 		self.demand = 0
 
 class Edge(object):
-	def __init__(self, i, j, weight):
+	def __init__(self, i, j, dist):
 		self.arc = (i,j)
-		self.weight = weight
+		self.dist = dist
+		self.pher = TAU0
 
 class Graph(object):
 	def __init__(self):
 		self.V = []
 		self.E = []
+
+	def importance(p, G, i, j):
+		return np.power(p*(1./G[i][j]), BETA)
+
+	def eq1(G, history):
+		j = -1
+		maxval = 0
+		for x in xrange(0,len(G.V)): # check every destination
+			if x + 1 not in history: # customer has not been visited yet
+				tmp = importance(G, history[-1]-1, x)
+				if tmp > maxval:
+					j = x + 1
+					maxval = tmp
+		return j
+
+	def eq2(G, pos):
+
 
 	def shortestPath(self, ant):
 		for index in np.argsort(G.adj[ant.pos-1]):
@@ -37,45 +61,36 @@ class Ant(object):
 		self.id = id
 		self.route = []
 
+	def selectPath(G):
+		selected = -1
+		if np.random.rand() <= Q0:
+			selected = eq1(G, self.route)
+		else:
+			selected = eq2(G, self.route)
+
+	def routeCost():
+		c = 0
+		for x in range(0,len(self.route)-1):
+			c += G.adj[self.route[x]-1][self.route[x+1]-1].dist
+		return c
+
 	def walk(self, G):
 		print "Walking"
 
-		self.pos = depot
-		self.route.append(self.pos)
-		self.supply = Ant.capacity
+		self.route.append(depot) # append to history/route
+		self.supply = Ant.capacity # set supply to max capacity
 
-		while len(set(self.route)) < dim:
-			self.pos = G.shortestPath(self)
-			if self.pos == depot:
-				self.supply = Ant.capacity
-			else:
-				self.supply -= G.V[self.pos-1].demand
-			self.route.append(self.pos)
-		self.route.append(depot)
+		while len(set(self.route)) < dim: # visit all customers
+			self.route.append(self.selectPath(G)) # append new customer to history/route
+			if self.route[-1] == depot: # new position is the depot
+				self.supply = Ant.capacity # set supply to max capacity
+			else: # new position is a customer
+				self.supply -= G.V[self.route[-1]-1].demand # subtract customer demand
+		self.route.append(depot) # after visiting all customers, return to the depot
+
 		print self.route
+		print self.cost()		
 
-		length = 0
-		for x in range(1,len(self.route)):
-			length += G.adj[self.route[x]-2][self.route[x]-1]
-		print length
-
-			
-
-
-# def randomGraph(n=20, p=0.25):
-# 	G = Graph()
-
-# 	for i in xrange(0,n):
-# 		G.V.add(Vertex(i))
-
-# 	for i in xrange(0,n):
-# 		for j in xrange(i,n):
-# 			#if np.random.rand() < p:
-# 			weight = np.random.rand()
-# 			G.E.add(Edge(i,j,weight))
-# 			G.E.add(Edge(j,i,weight))
-
-# 	return G
 
 def generateGraphFrom(data):
 	global depot, dim
@@ -96,10 +111,10 @@ def generateGraphFrom(data):
 	for i in xrange(0,dim):
 		for j in xrange(i,dim):
 			if i == j:
-				G.adj[i][j] = float('inf')
+				G.adj[i][j] = Edge(i, j, float('inf'))
 			else:
-				G.adj[i][j] = TSPLIBParser.d_euc2d(G.V[i].coords, G.V[j].coords)
-				G.adj[j][i] = TSPLIBParser.d_euc2d(G.V[i].coords, G.V[j].coords)
+				G.adj[i][j] = Edge(i, j, TSPLIBParser.d_euc2d(G.V[i].coords, G.V[j].coords))
+				G.adj[j][i] = Edge(j, i, TSPLIBParser.d_euc2d(G.V[i].coords, G.V[j].coords))
 
 	return G
 
@@ -125,36 +140,7 @@ def drawGraph(G):
 	plt.axis('off')
 	plt.show()
 
-def walkingAnt(G, i):
-	print "Walking"
-
-	currentNode = depot
-	nodesSeen = []
-	amountDelivered = 0
-	route = []
-
-	nodesSeen.append(depot)
-
-	while amountDelivered < Ant.capacity:
-		randomNode = randint(0, len(G.V))
-		if (randomNode not in nodesSeen and randomNode != currentNode):
-			amountDelivered += 5 #value of edge between node i and j
-			route.append(randomNode)
-			nodesSeen.append(randomNode) 
-			currentNode = randomNode
-		else:
-			break	
-	print amountDelivered
-	print route
-	print nodesSeen
-
-
-
 if __name__ == '__main__':
-	beta = 2.3
-	alpha = 0.1
-	numberOfAnts = 2
-
 	visualize = False
 	data = TSPLIBParser('instances/A/A-n32-k5.vrp').parse()
 	G = generateGraphFrom(data)
@@ -164,7 +150,7 @@ if __name__ == '__main__':
 
 	# Create a list of pre-set length containing the ants
 	ants = []
-	for i in xrange(0, numberOfAnts):
+	for i in xrange(0, M):
 		ants.append(Ant(i))
 
 	for ant in ants:
